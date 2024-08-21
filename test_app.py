a22b638afb66b16d0590bf0368993fcfb8e5a394
srijana39 <srijana.aryal@student.torrens.edu.au>
2024-08-21 15:46:21 +1000

    correct failed test

--- test_app.py
+++ test_app.py
@@ -9,8 +9,8 @@ def test_add_negative():
 def test_subtract_positive():
     assert subtract(5, 3) == 2  # This test will pass
 
-def test_add_fail():
-    assert add(1, 1) == 3  # This test will fail
+def test_add_corrected():
+    assert add(1, 1) == 2  # Corrected to pass
 
-def test_subtract_fail():
-    assert subtract(10, 5) == 8  # This test will fail
+def test_subtract_corrected():
+    assert subtract(10, 5) == 5  # Corrected to pass
